53efffae5341984ba265c7bb57fdb17465b551e1
CCVpopular <datkhodo888@gmail.com>
2024-06-16 14:38:36 +0700

    first commit

---
+++ src/test/java/com/hutech/doanjava/DoAnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.doanjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
