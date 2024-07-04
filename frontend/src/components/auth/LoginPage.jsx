bb203def8a143f3c4e1ae37822cfcec658c2cd3c
Phune23 <phutranbs23@gmail.com>
2024-07-04 15:52:22 +0700

    Write form into Vietnamese.

--- frontend/src/components/auth/LoginPage.jsx
+++ frontend/src/components/auth/LoginPage.jsx
@@ -43,34 +43,35 @@ const handleSubmit = async (e) => {
                 </span>
                 <div class="form-box login">
                     <div class="form-details">
-                        <h2>Wellcome Back</h2>
-                        <p>Please log in to start messaging.</p>
+                        <h2>Chào mừng !</h2>
+                        <p>Để bắt đầu "CHAT CHIT" vui lòng đăng nhập tài khoản.</p>
                     </div>
                     <div class="form-content">
-                        <h2>LOGIN</h2>
+                        <h2>Đăng ký</h2>
                         {error && <p className="error-message">{error}</p>}
                         <form onSubmit={handleSubmit}>
                             <div className="input-field">
                                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                                <label>Email: </label>
+                                <label>Email </label>
                             </div>
                             <div className="input-field">
                                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                                <label>Password: </label>
+                                <label>Mật Khẩu </label>
                             </div>
-                            <a href="https://github.com/CCVpopular/DoAnjava" class="forgot-pass">Forgot password</a>
-                            <button type="submit">Login</button>
+                            <button type="submit">Đăng nhập</button>
                             <button class="loginBtn loginBtn--facebook">
-                                Login with Facebook
+                                Đăng nhập Facebook
                             </button>
                             <button class="loginBtn loginBtn--google">
-                                Login with Google
+                                Đăng nhập Google
                             </button>
                         </form>
-                        <button><Link to={`/forgotPassword`}>Quen Mat Khau</Link></button>
+                        <div className="divForgotPassword">
+                            <button class="forgotPasswordBtn"><Link to={`/forgotPassword`}>Quên Mật Khẩu</Link></button>
+                        </div>
                         <div class="bottom-link">
-                            Don't have an account?
-                            <a href="https://github.com/CCVpopular/DoAnjava" id="signup-link">Signup</a>
+                            Bạn có tài khoản không ?
+                            <a href="https://github.com/CCVpopular/DoAnjava" id="signup-link"> Tạo tài khoản mới</a>
                         </div>
                     </div>
                 </div>
